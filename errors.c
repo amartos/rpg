dc6d66373a668984cfa0024efe4cb495ee657ddd
Alexandre Martos <alexandre.martos@protonmail.ch>
2019-08-25 00:23:41 +0200

    Implement errors logging
    Two modules have been added to handle errors. The first (errors) defines
    and log the errors - for now the log is limited to stdout print - ;
    the second is an already made module defining macros to TRY, THROW and
    CATCH errors, made by Francesco Nidito.

---
+++ errors.c
@@ -0,0 +1,24 @@
+#include "errors.h"
+
+// this is only temporary, to change in log in file
+void printe(const time_t now, const int error_id, 
+        const char error_def[], const char sdl_message[])
+{
+    printf("[%ld] Error %d (%s): %s\n", now, error_id, 
+            error_def, sdl_message);
+}
+
+void logger(const int error_id, const char sdl_message[])
+{
+    time_t now;
+    time(&now);
+    switch(error_id)
+    {
+        case SCREEN_INIT_FAILURE:
+            printe(now, error_id, "SDL init failed", sdl_message);
+            break;
+        case VIDEO_MODE_LOAD_FAILURE:
+            printe(now, error_id, "video mode loading failed", sdl_message);
+            break;
+    }
+}
---
+++ errors.h
@@ -0,0 +1,17 @@
+#ifndef RPG_ERRORS
+#define RPG_ERRORS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// prototypes
+void logger(const int error_id, const char sdl_message[]);
+
+// custom errors
+#define SCREEN_INIT_FAILURE 1
+#define VIDEO_MODE_LOAD_FAILURE 2
+
+
+
+#endif // define RPG_ERROR
