ff2ace92a25d0ab4aba9d6011aea088f0a8e8fdf
Alexandre Martos <alexandre.martos@protonmail.ch>
2019-09-17 11:22:25 +0200

    Set own macros for PF test unit
    As TILES_WIDTH and TILES_HEIGHT have changed to be adapted to isometric
    projection, the PF test unit had to be adapted. Instead of making an
    isometric projection too in the unit, which is not the point of the
    test, own width and height were defined, and the whole unit adapted.
    STARTX and Y were also defined for consistency.

--- test/pathfinding_test.c
+++ test/pathfinding_test.c
@@ -1,6 +1,5 @@
 #include "../errors.h"
 #include "../try_throw_catch.h"
-#include "../macros.h"
 
 #include "../coord.h"
 #include "../pathfinding.h"
@@ -11,6 +10,10 @@
 #include <SDL2/SDL_image.h>
 #include <math.h>
 
+#define STARTX 5
+#define STARTY 5
+#define TEST_TILES_WIDTH 32
+#define TEST_TILES_HEIGHT 32
 
 int main(int argc, char *argv[])
 {
@@ -49,45 +52,45 @@ int main(int argc, char *argv[])
 
     SDL_Rect infos;
     infos.x = 0; infos.y = 0;
-    infos.w = 32; infos.h = 32;
+    infos.w = TEST_TILES_WIDTH; infos.h = TEST_TILES_HEIGHT;
 
     SDL_Texture* rainbow[MAX_PATH_NODES];
 
-    SDL_Surface *black_rect_surface = SDL_CreateRGBSurface(0, TILES_WIDTH, TILES_HEIGHT, SCREEN_BPP, 0, 0, 0, 0);
+    SDL_Surface *black_rect_surface = SDL_CreateRGBSurface(0, TEST_TILES_WIDTH, TEST_TILES_HEIGHT, SCREEN_BPP, 0, 0, 0, 0);
     SDL_FillRect(black_rect_surface, NULL, SDL_MapRGB(black_rect_surface->format, 0, 0, 0));
     black_rect_surface = SDL_ConvertSurfaceFormat(black_rect_surface, SDL_PIXELFORMAT_RGBA8888, 0);
     SDL_Texture *black_rect = SDL_CreateTextureFromSurface(renderer, black_rect_surface);
     SDL_FreeSurface(black_rect_surface);
 
-    SDL_Surface *grey10_rect_surface = SDL_CreateRGBSurface(0, TILES_WIDTH, TILES_HEIGHT, SCREEN_BPP, 0, 0, 0, 0);
+    SDL_Surface *grey10_rect_surface = SDL_CreateRGBSurface(0, TEST_TILES_WIDTH, TEST_TILES_HEIGHT, SCREEN_BPP, 0, 0, 0, 0);
     SDL_FillRect(grey10_rect_surface, NULL, SDL_MapRGB(grey10_rect_surface->format, 155, 155, 155));
     grey10_rect_surface = SDL_ConvertSurfaceFormat(grey10_rect_surface, SDL_PIXELFORMAT_RGBA8888, 0);
     SDL_Texture *grey10_rect = SDL_CreateTextureFromSurface(renderer, grey10_rect_surface);
     SDL_FreeSurface(grey10_rect_surface);
 
-    SDL_Surface *grey5_rect_surface = SDL_CreateRGBSurface(0, TILES_WIDTH, TILES_HEIGHT, SCREEN_BPP, 0, 0, 0, 0);
+    SDL_Surface *grey5_rect_surface = SDL_CreateRGBSurface(0, TEST_TILES_WIDTH, TEST_TILES_HEIGHT, SCREEN_BPP, 0, 0, 0, 0);
     SDL_FillRect(grey5_rect_surface, NULL, SDL_MapRGB(grey5_rect_surface->format, 205, 205, 205));
     grey5_rect_surface = SDL_ConvertSurfaceFormat(grey5_rect_surface, SDL_PIXELFORMAT_RGBA8888, 0);
     SDL_Texture *grey5_rect = SDL_CreateTextureFromSurface(renderer, grey5_rect_surface);
     SDL_FreeSurface(grey5_rect_surface);
 
-    SDL_Surface *green_rect_surface = SDL_CreateRGBSurface(0, TILES_WIDTH/2, TILES_HEIGHT/2, SCREEN_BPP, 0, 0, 0, 0);
+    SDL_Surface *green_rect_surface = SDL_CreateRGBSurface(0, TEST_TILES_WIDTH/2, TEST_TILES_HEIGHT/2, SCREEN_BPP, 0, 0, 0, 0);
     SDL_FillRect(green_rect_surface, NULL, SDL_MapRGB(green_rect_surface->format, 0, 128, 0));
     green_rect_surface = SDL_ConvertSurfaceFormat(green_rect_surface, SDL_PIXELFORMAT_RGBA8888, 0);
     SDL_Texture *green_rect = SDL_CreateTextureFromSurface(renderer, green_rect_surface);
     SDL_FreeSurface(green_rect_surface);
 
-    SDL_Surface *start_rect_surface = SDL_CreateRGBSurface(0, TILES_WIDTH, TILES_HEIGHT, SCREEN_BPP, 0, 0, 0, 0);
+    SDL_Surface *start_rect_surface = SDL_CreateRGBSurface(0, TEST_TILES_WIDTH, TEST_TILES_HEIGHT, SCREEN_BPP, 0, 0, 0, 0);
     SDL_FillRect(start_rect_surface, NULL, SDL_MapRGB(start_rect_surface->format, 0, 255, 0));
     start_rect_surface = SDL_ConvertSurfaceFormat(start_rect_surface, SDL_PIXELFORMAT_RGBA8888, 0);
     SDL_Texture *start_rect = SDL_CreateTextureFromSurface(renderer, start_rect_surface);
     SDL_FreeSurface(start_rect_surface);
 
     SDL_Rect start;
-    start.x = 5*32; start.y = 5*32;
-    start.w = 32; start.h = 32;
+    start.x = STARTX*TEST_TILES_WIDTH; start.y = STARTY*TEST_TILES_HEIGHT;
+    start.w = TEST_TILES_WIDTH; start.h = TEST_TILES_HEIGHT;
     Coord start_units; init_coord(&start_units);
-    start_units.x = 5; start_units.y = 5;
+    start_units.x = STARTX; start_units.y = STARTY;
 
     Coord coord; init_coord(&coord);
     Coord max_coord; init_coord(&max_coord);
@@ -183,8 +186,8 @@ int main(int argc, char *argv[])
                 switch(event.button.button)
                 {
                     case SDL_BUTTON_LEFT:
-                        click.x = event.button.x / TILES_WIDTH;
-                        click.y = event.button.y / TILES_HEIGHT;
+                        click.x = event.button.x / TEST_TILES_WIDTH;
+                        click.y = event.button.y / TEST_TILES_HEIGHT;
                         nodes = find_path(
                                 path,
                                 start_units, click,
@@ -209,7 +212,7 @@ int main(int argc, char *argv[])
         for (j=0;j<max_coord.y;j++)
             for (i=0;i<max_coord.x;i++)
             {
-                infos.x = i * 32; infos.y = j * 32;
+                infos.x = i * TEST_TILES_WIDTH; infos.y = j * TEST_TILES_HEIGHT;
                 if (collision[j][i])
                     SDL_RenderCopy(renderer, black_rect, NULL, &infos);
                 else if (cost[j][i])
@@ -230,7 +233,7 @@ int main(int argc, char *argv[])
 	    for (i=0;i<=max;i++)
 	    {
                 score = 255 * i / max;
-                SDL_Surface *temp = SDL_CreateRGBSurface(0, TILES_WIDTH, TILES_HEIGHT, SCREEN_BPP, 0, 0, 0, 0);
+                SDL_Surface *temp = SDL_CreateRGBSurface(0, TEST_TILES_WIDTH, TEST_TILES_HEIGHT, SCREEN_BPP, 0, 0, 0, 0);
                 SDL_FillRect(temp, NULL, SDL_MapRGB(temp->format, score, 0, 255 - score));
                 temp = SDL_ConvertSurfaceFormat(temp, SDL_PIXELFORMAT_RGBA8888, 0);
                 rainbow[i] = SDL_CreateTextureFromSurface(renderer, temp);
@@ -243,18 +246,18 @@ int main(int argc, char *argv[])
                     ncurrent = max_coord.x * j + i;
                     if (scores[ncurrent])
                     {
-                        infos.x = i * 32; infos.y = j * 32;
+                        infos.x = i * TEST_TILES_WIDTH; infos.y = j * TEST_TILES_HEIGHT;
                         SDL_RenderCopy(renderer, rainbow[scores[ncurrent]], NULL, &infos);
                     }
                 }
-            infos.w = 16; infos.h = 16;
+            infos.w = TEST_TILES_WIDTH/2; infos.h = TEST_TILES_HEIGHT/2;
             for (i=0;i<nodes;i++)
             {
-                infos.x = path[i].x * 32 + 8; infos.y = path[i].y * 32 + 8;
+                infos.x = path[i].x * TEST_TILES_WIDTH + TEST_TILES_WIDTH/4; infos.y = path[i].y * TEST_TILES_HEIGHT + TEST_TILES_HEIGHT/4;
                 SDL_RenderCopy(renderer, green_rect, NULL, &infos);
             }
             nodes = 0;
-            infos.w = 32; infos.h = 32;
+            infos.w = TEST_TILES_WIDTH; infos.h = TEST_TILES_HEIGHT;
             for (i=0;i<MAX_PATH_NODES;i++)
                 scores[i] = 0;
         }
