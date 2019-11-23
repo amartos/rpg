e1e283b8e20b1a20ec988fe30101fab69c9e568a
Alexandre Martos <alexandre.martos@protonmail.ch>
2019-11-23 17:36:15 +0100

    Split the PF algo from the module main function
    A* function was embedded into the find_path function, which was not
    optimal for improvement.
    
    A* has now been given its own function, and has been converted to
    Dijkstra with a boolean (TRUE = add cost of distance), as A* is just
    Disjktra + cost depending on distance to goal.

--- pathfinding.c
+++ pathfinding.c
@@ -141,76 +141,45 @@ static void get_neighbours(Coord next[8], Coord const current, Coord const max_c
 static unsigned int calculate_cost(
         unsigned int current_cost,
         Coord const next_coord,
-        Coord const goal,
         unsigned int** const cost_map
         )
 {
-    unsigned int next_cost = 0;
     unsigned int x = next_coord.x, y = next_coord.y;
-    int distance = abs(goal.x - x) + abs(goal.y - y);
-
-    next_cost = 1 + current_cost + distance + cost_map[y][x];
+    unsigned int next_cost = 1 + current_cost + cost_map[y][x];
     return next_cost;
 }
 
-unsigned int find_path(
-        Coord path[MAX_PATH_NODES],
-        Coord const precise_start, Coord const precise_goal,
+static void dijkstra(
+        Bool a_star,
+        unsigned int cost[],
+        unsigned int came_from[],
+        Coord conversion[],
+        Coord start, Coord goal,
         Coord const max_coord,
         unsigned int** const collision_map,
-        unsigned int** const cost_map,
-        unsigned int scores[]
+        unsigned int** const cost_map
         )
 {
-    // vars init
-    unsigned int nodes = 0;
-
-    unsigned int x = precise_start.x, y = precise_start.y;
-    Coord start; init_coord(&start);
-    start.x = x; start.y = y;
-
-    Coord goal; init_coord(&goal);
-    x = precise_goal.x; y = precise_goal.y;
-    goal.x = x; goal.y = y;
-
-    // all this can be put in the first if statement for optimisation
-    unsigned int i = 0, j = 0, n = 0, nnext = 0, ncurrent = 0;
+    unsigned int i = 0, nnext = 0, ncurrent = 0, new_cost = 0;
     unsigned int nstart = convert_coord_to_number(start, max_coord);
     unsigned int ngoal = convert_coord_to_number(goal, max_coord);
     unsigned int max_array = max_coord.x * max_coord.y;
-    unsigned int queue[max_array], cost[max_array+1], came_from[max_array+1];
-    unsigned int new_cost = 0;
+    unsigned int queue[max_array];
+    for (i=0;i<max_array;i++)
+        queue[i] = 0;
     Bool done = FALSE;
 
     Coord current; init_coord(&current);
     Coord next; init_coord(&next);
     Coord coord; init_coord(&coord);
     Coord all_next[8];
-    Coord conversion[max_array+1];
-
     for (i=N;i<=NW;i++)
         init_coord(&all_next[i]);
-    for (i=0;i<max_array;i++)
-        queue[i] = 0;
-    for (i=0;i<max_array+1;i++)
-    {
-        init_coord(&conversion[i]);
-        cost[i] = 0;
-        came_from[i] = 0;
-    }
-    for (i=0;i<max_coord.x;i++)
-        for (j=0;j<max_coord.y;j++)
-        {
-            coord.x = i, coord.y = j;
-            n = convert_coord_to_number(coord, max_coord);
-            conversion[n] = coord;
-        }
 
     came_from[nstart] = nstart;
     queue[0] = nstart;
     cost[nstart] = 0;
 
-    // A* starts here
     while (!done)
     {
         ncurrent = queue[0];
@@ -227,7 +196,11 @@ unsigned int find_path(
                     )
                 {
                     nnext = convert_coord_to_number(all_next[i], max_coord);
-                    new_cost = calculate_cost(cost[ncurrent], all_next[i], goal, cost_map);
+                    new_cost = calculate_cost(cost[ncurrent], all_next[i], cost_map);
+                    if (a_star)
+                        // Converts Dijkstra in A* : add cost f(distance)
+                        new_cost += abs(goal.x - all_next[i].x) + abs(goal.y - all_next[i].y);
+
                     if (!came_from[nnext] || new_cost < cost[nnext])
                     {
                         came_from[nnext] = ncurrent;
@@ -254,16 +227,59 @@ unsigned int find_path(
         else
             done = TRUE;
     }
+}
+
+unsigned int find_path(
+        Coord path[MAX_PATH_NODES],
+        Coord const precise_start, Coord const precise_goal,
+        Coord const max_coord,
+        unsigned int** const collision_map,
+        unsigned int** const cost_map,
+        unsigned int scores[]
+        )
+{
+    unsigned int nodes = 1, i,j,n;
+
+    unsigned int x = precise_start.x, y = precise_start.y;
+    Coord start; init_coord(&start);
+    start.x = x; start.y = y;
+
+    Coord goal; init_coord(&goal);
+    x = precise_goal.x; y = precise_goal.y;
+    goal.x = x; goal.y = y;
+
+    unsigned int nstart = convert_coord_to_number(start, max_coord);
+    unsigned int ngoal = convert_coord_to_number(goal, max_coord);
+    unsigned int ncurrent = ngoal;
+    unsigned int max_array = max_coord.x * max_coord.y;
+
+    unsigned int cost[max_array+1], came_from[max_array+1];
+    Coord conversion[max_array+1], coord;
+
+    for (i=0;i<max_array+1;i++)
+    {
+        init_coord(&conversion[i]);
+        cost[i] = 0;
+        came_from[i] = 0;
+    }
+
+    for (i=0;i<max_coord.x;i++)
+        for (j=0;j<max_coord.y;j++)
+        {
+            coord.x = i, coord.y = j;
+            n = convert_coord_to_number(coord, max_coord);
+            conversion[n] = coord;
+        }
+
+    //A*
+    dijkstra(TRUE, cost, came_from, conversion, start, goal, max_coord, collision_map, cost_map);
 
     path[0] = precise_goal;
-    nodes = 1;
-    ncurrent = ngoal;
-
     for (i=1;i<MAX_PATH_NODES;i++)
     {
         nodes++;
-        path[i] = conversion[ncurrent]; // came_from[goal] = previous
-        ncurrent = came_from[ncurrent];
+        path[i] = conversion[ncurrent];
+        ncurrent = came_from[ncurrent]; // came_from[goal] = previous
         if (ncurrent == nstart)
         {
             if (i<MAX_PATH_NODES-1 && !is_same_coord(start, precise_start))
